a17d9007f5859057b08aef0a24dcf400122807bf
Albin Ahlström <ahlstrom.albin@gmail.com>
2024-02-25 01:29:25 +0100

    get results

--- tool.py
+++ tool.py
@@ -2,8 +2,12 @@ import requests
 
 
 def get_names(drivers):
-    print(drivers)
+    print(drivers[0])
     return [driver["abbreviation"] for driver in drivers]
+
+
+def get_results(drivers):
+    return [driver["race_results"] for driver in drivers]
 
 
 driver_url = "https://f1fantasytoolsapi-szumjzgxfa-ew.a.run.app/race-results/driver?season=2023"
@@ -13,6 +17,6 @@ if response.status_code != 200:
     raise Exception(f"failed to fetch url. {response.status_code}")
 
 drivers = response.json()
-points = get_names(drivers)
+points = get_results(drivers)[0]
 
 print(points)
