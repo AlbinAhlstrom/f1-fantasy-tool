12b85b4bbbbd9b206a65b5e5ff6c9dd35037fc88
Albin Ahlström <ahlstrom.albin@gmail.com>
2024-02-25 01:46:03 +0100

    get ids

--- tool.py
+++ tool.py
@@ -1,13 +1,15 @@
 import requests
 
 
-def get_names(drivers):
-    print(drivers[0])
-    return [driver["abbreviation"] for driver in drivers]
+def get_ids(drivers):
+    return {driver["abbreviation"]: id for id, driver in enumerate(drivers)} 
 
 
 def get_results(drivers):
     return [driver["race_results"] for driver in drivers]
+
+def get_driver_results(name, results):
+    print()
 
 
 driver_url = "https://f1fantasytoolsapi-szumjzgxfa-ew.a.run.app/race-results/driver?season=2023"
@@ -17,6 +19,6 @@ if response.status_code != 200:
     raise Exception(f"failed to fetch url. {response.status_code}")
 
 drivers = response.json()
-points = get_results(drivers)[0]
+points = get_ids(drivers)
 
 print(points)
